0b1fbb27079f800af58264ba2fada5a7063ba52c
yamayumasan <yuma2066@gmail.com>
2026-02-23 00:27:56 +0900

    auto: iteration 3 checkpoint

--- packages/mobile/jest.config.js
+++ packages/mobile/jest.config.js
@@ -1,13 +1,16 @@
 /** @type {import('jest').Config} */
+const jestExpoPreset = require('jest-expo/jest-preset')
+
 module.exports = {
-  preset: 'jest-expo',
+  ...jestExpoPreset,
   testEnvironment: 'node',
+  // Override setupFiles to exclude react-native's ESM setup.js
+  setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@machi/shared)',
+    '/node_modules/(?!(.pnpm/)?(@?react-native|react-native|@react-native|@react-native-community|jest-react-native|expo|@expo|@expo-google-fonts|react-navigation|@react-navigation|@sentry|native-base|react-native-svg|@machi|axios))',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.test.{ts,tsx}'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.test.{ts,tsx}',
@@ -18,6 +21,7 @@ module.exports = {
   coverageReporters: ['text', 'lcov', 'html'],
   clearMocks: true,
   moduleNameMapper: {
+    ...jestExpoPreset.moduleNameMapper,
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@machi/shared$': '<rootDir>/../shared/dist/index.js',
   },
